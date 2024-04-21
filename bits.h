6d249ed7578288038a48ad3a987bc609c806d7c4
FahdTolba <FahdTolba@users.noreply.github.com>
2024-04-21 13:40:15 +0200

    first commit

---
+++ bits.h
@@ -0,0 +1,21 @@
+#ifndef __BITS_H
+#define __BITS_H
+
+
+#define MIN(A,B) A >= B ? A : B
+
+void reverse_endianness(){
+
+}
+
+void
+big_endian(){
+
+}
+
+int
+host_endian(int){
+
+}
+
+#endif /* __BITS_H */
---
+++ math_lib.h
@@ -0,0 +1,118 @@
+#ifndef __MATH_LIB_H
+#define __MATH_LIB_H
+
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+
+#include"bits.h"
+
+using namespace std;
+
+class Arith_Entity{
+public:
+	virtual void operator = (int) = 0;
+//	void op ++() = 0; //how would define increment for a polynomial or Matrix??
+				//	addition to the field unity element ?
+
+//	void op ++(int) = 0;//postfix version
+
+//	Arith_Entity op + (Arith_Entity) = 0;
+//	Arith_Entity op - (Arith_Entity) = 0;
+//	Arith_Entity op / (Arith_Entity) = 0;
+//	Arith_Entity op * (Arith_Entity) = 0;
+
+/*
+	+= // if + is overloaded does that implicitly overload += ??
+	-=
+	*=
+	/=
+	==
+*/
+};
+
+class Number : public Arith_Entity{
+
+	private:
+
+	Number(); //no ambiguity please
+
+//	Number(Number&); // no need to disable automatic copy for now
+//	Number operator = (Number N); //no need to disable automatic copy for now
+
+	int size; //size in bytes
+	char *data;	//big-endian (network-byte order)
+	int tmp;
+
+//	Number operator = (Number N); //no need to disable automatic copy for now
+
+	
+	public:
+
+
+	Number(int bsize) : size(bsize){ 	printf("constructor called: size is %d\n",size);
+		data = (char *)malloc(size);
+		memset(data, 0, size);
+	}
+
+	void operator = (Number N){ //override automatic copy
+
+		if(size != N.size){
+			data = (char *)realloc(data, N.size);
+		}
+		size = N.size;
+	
+		if(data && N.data)
+			memcpy(data, N.data, size);
+		else
+			printf("no memory copied\n");
+			//exception
+	}
+
+	void operator = (int n){ 
+		//tmp = big_endian(n); //we use big_endian
+		//memcpy(data, tmp, size);
+		*data = n;
+	}
+
+	bool operator == (int temp){
+		return !memcmp(data, &temp, sizeof(temp));
+	}
+
+	bool operator == (Number N2) {
+		return size == N2.size && (!memcmp(data, N2.data, size));
+	}
+
+	bool identical (Number N2){
+		return *this == N2 && data == N2.data; 
+	}
+
+	void print(){
+		if(size > sizeof(long)){
+			cout<<"print()ing numbers larger than 8 bytes currently not supported"<<endl;
+			//first numbers in hexadecimal representation
+			//then support decimal printing
+			//Itoa(), requires modulo division,
+			//which requires division and subtraction
+			//so we will to define a generic version of itoa() ( Itoa() )
+		}else{
+			cout<<"size is "<<size<<endl;
+			cout<<"data is "<<(int)host_endian(*data)<<endl;
+			printf("printf(): data is %d\n", *data);
+			printf("data is %p and holding value %d\n", data, *data);
+		}
+	}	
+};
+
+
+class Polynomial : public Arith_Entity{
+
+//	op +()	//addition to the finite field's unity element
+//	public:
+};
+
+//class Matrix : Arith_Entity{
+//      op +()  //addition to the finite field's unity element
+//};
+
+#endif //__MATH_LIB_H
